7e54e4ef9b7adf481a1bdcd2eff6ef8737b7f609
Keilyn Valeria Garita González <kgaritagonzalez@gmail.com>
2025-11-22 16:06:49 -0600

    modificacion de prueba
    Se modifica un test para que pueda fallar al propio

--- Examen2PruebasUnitarias.py
+++ Examen2PruebasUnitarias.py
@@ -10,8 +10,9 @@ class TestMiClase(unittest.TestCase):
     # Pruebas para ObtieneValencia
     def test_obtiene_valencia_con_varios_impares(self):
         resultado = self.objeto.ObtieneValencia(1234567)
-        self.assertEqual(resultado, 4)  # 1, 3, 5, 7 son impares
-    
+        #self.assertEqual(resultado, 4)  # 1, 3, 5, 7 son impares
+        self.assertEqual(resultado, 5)  # <-- A PROPÓSITO MAL
+
     def test_obtiene_valencia_solo_pares(self):
         resultado = self.objeto.ObtieneValencia(2468)
         self.assertEqual(resultado, 0)  # No hay dígitos impares
