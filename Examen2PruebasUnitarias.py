990f0c8dd02e8283f5a8146f34ab4a32e9b7bfea
miguelvaldmart <miguelvaldmart@gmail.com>
2025-11-22 16:16:02 -0600

    Prueba de error

--- Examen2PruebasUnitarias.py
+++ Examen2PruebasUnitarias.py
@@ -11,7 +11,7 @@ class TestMiClase(unittest.TestCase):
     def test_obtiene_valencia_con_varios_impares(self):
         resultado = self.objeto.ObtieneValencia(1234567)
         #self.assertEqual(resultado, 4)  # 1, 3, 5, 7 son impares
-        self.assertEqual(resultado, 5)  # <-- A PROPÓSITO MAL
+        self.assertEqual(resultado, 9)  # <-- A PROPÓSITO MAL
 
     def test_obtiene_valencia_solo_pares(self):
         resultado = self.objeto.ObtieneValencia(2468)
